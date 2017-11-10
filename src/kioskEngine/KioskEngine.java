954c3abaf66eb78943154c11741069a8f6c720c4
vojta mosby <wvmosby@wpi.edu>
2017-11-09 20:39:58 -0500

    filled out the Engine and staff also added a git ignore

---
+++ src/kioskEngine/KioskEngine.java
@@ -0,0 +1,22 @@
+
+package kioskEngine;
+
+public class KioskEngine{
+    private HospitalMap map;
+
+    public KioskEngine(){
+
+    }
+
+    public void addNode(){
+
+    }
+
+    public List<Node> findPath(){
+
+    }
+
+    public void requestService(){
+
+    }
+}
---
+++ src/service/Staff.java
@@ -0,0 +1,58 @@
+
+package service;
+
+public class Staff{
+    private String username;
+    private String password;
+    private String jobTitle;
+    private String fullName;
+    private int ID;
+
+    public Staff(String username, String password, String jobTitle, String fullName, int ID){
+
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public int getID(){
+        return ID
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void changePassword(String newPass, String oldPass){
+
+    }
+}
