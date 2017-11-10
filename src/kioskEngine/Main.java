4b703b55d5cab35661028e2241b58444315f7576
Travis Norris <TravisYNorris@gmail.com>
2017-11-09 23:21:27 -0500

    Adding all screens to main and connecting login buttons

--- src/kioskEngine/Main.java
+++ src/kioskEngine/Main.java
@@ -14,10 +14,17 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         ScreenController myScreenController = new ScreenController();
-        myScreenController.loadScreen(ScreenController.MainID, ScreenController.MainFile);
-        myScreenController.loadScreen(ScreenController.LoginID, ScreenController.LoginFile);
+        myScreenController.loadScreen(ScreenController.AddNodeID, ScreenController.AddNodeFile);
+        myScreenController.loadScreen(ScreenController.AdminMenuID, ScreenController.AdminMenuFile);
         myScreenController.loadScreen(ScreenController.FilterID, ScreenController.FilterFile);
+        myScreenController.loadScreen(ScreenController.LoginID, ScreenController.LoginFile);
+        myScreenController.loadScreen(ScreenController.LogoutID, ScreenController.LogoutFile);
+        myScreenController.loadScreen(ScreenController.MainID, ScreenController.MainFile);
+        //myScreenController.loadScreen(ScreenController.NodeConfirmID, ScreenController.NodeConfirmFile);
         myScreenController.loadScreen(ScreenController.PathID, ScreenController.PathFile);
+        myScreenController.loadScreen(ScreenController.RequestID, ScreenController.RequestFile);
+        myScreenController.loadScreen(ScreenController.ServiceChoiceID, ScreenController.ServiceChoiceFile);
+        //myScreenController.loadScreen(ScreenController.ThankYouID, ScreenController.ThankYouFile);
 
         myScreenController.setScreen(ScreenController.MainID);
 
