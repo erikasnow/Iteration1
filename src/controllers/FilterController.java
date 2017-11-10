4719c8d075c8c3c3e285d06501a624dbc0bfad08
Ayuzvik <ayuzvik@wpi.edu>
2017-11-09 21:27:40 -0500

    Added FXML files and Merged Controller Files

---
+++ src/controllers/FilterController.java
@@ -0,0 +1,18 @@
+package Filter;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+
+public class FilterController {
+
+    @FXML
+    private CheckBox checkBoxBathrooms;
+
+    @FXML
+    private CheckBox checkBoxExits;
+
+    @FXML
+    private Button btncancel;
+
+}
