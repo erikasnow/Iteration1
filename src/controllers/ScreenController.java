3025d29eb6bdba29f3b702feb4d523a299b4913c
Travis Norris <TravisYNorris@gmail.com>
2017-11-09 21:56:27 -0500

    Implementing ScreenController methods

--- src/controllers/ScreenController.java
+++ src/controllers/ScreenController.java
@@ -1,11 +1,15 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.layout.StackPane;
 
 import java.util.HashMap;
 
 public class ScreenController extends StackPane {
+
     private HashMap<String, Node> screens = new HashMap<String, Node>();
 
     public ScreenController(){
@@ -14,28 +18,47 @@ public class ScreenController extends StackPane {
 
     //add a new screen to the screens HashMap
     public void addScreen(String name, Node screen){
-
+        screens.put(name,screen);
     }
 
     //return a screen from the screens HashMap
     public Node getScreen(String name){
-        return new ScreenController(); //stub for function headers
+        return screens.get(name); //stub for function headers
     }
 
     //load a screen into the overall ui.
     //does not display the screen
-    public boolean loadScreen(String name){
-        return true; //stub for function headers
+    public boolean loadScreen(String name, String file){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(file));
+            Parent fxmlToLoad = (Parent) fxmlLoader.load();
+            ControllableScreen controllerToLoad = ((ControllableScreen) fxmlLoader.getController());
+            controllerToLoad.setParentController(this);
+            addScreen(name, fxmlToLoad);
+            return true;
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+
     }
 
     //set the screen currently being displayed on the pane
     public boolean setScreen(String name){
-        return true; //stub for function header
+        if(screens.containsKey(name)){
+            if(!getChildren().isEmpty()){
+                getChildren().remove(0);
+            }
+            getChildren().add(screens.get(name));
+            return true;
+        }
+        return false;
     }
 
     //unloads a screen from the ui
-    public boolean unloadScreen(String name){
-        return true; //stub for function header
+    public boolean unloadScreen(String name) {
+        screens.remove(name);
+        return true;
     }
-
 }
